70d52da6189178b2c1e5322c9882ba91d27e24bd
Alsey Coleman Miller <alseycmiller@gmail.com>
2016-04-20 18:41:28 -0500

    Added iOS test target

---
+++ Xcode/TestApp/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  TestApp
+//
+//  Created by Alsey Coleman Miller on 4/20/16.
+//  Copyright © 2016 PureSwift. All rights reserved.
+//
+
+import UIKit
+import GATT
+
+class ViewController: UIViewController {
+    
+    let central = CentralManager()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        central.waitForPoweredOn()
+        
+        let duration = 5
+        
+        print("Scanning for \(duration) seconds")
+        
+        let scanResults = central.scan()
+        
+        print("Found \(scanResults.count) peripherals")
+    }
+}
---
+++ Xcode/TestApp/main.swift
@@ -0,0 +1,13 @@
+//
+//  main.swift
+//  TestApp
+//
+//  Created by Alsey Coleman Miller on 4/19/16.
+//  Copyright © 2016 ColemanCDA. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import GATT
+
+UIApplicationMain(Process.argc, nil, nil, NSStringFromClass(AppDelegate))
