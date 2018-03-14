999465f9b7bea3626f1bcd53304fad12eb1336a0
Alsey Coleman Miller <alseycmiller@gmail.com>
2018-03-14 17:50:06 -0500

    Added test iOS and Mac utilities

---
+++ Xcode/BluetoothExplorer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BluetoothExplorer
+//
+//  Created by Alsey Coleman Miller on 3/14/18.
+//  Copyright © 2018 PureSwift. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Xcode/GATTTool/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  GATTTool
+//
+//  Created by Alsey Coleman Miller on 3/11/18.
+//  Copyright © 2018 PureSwift. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
