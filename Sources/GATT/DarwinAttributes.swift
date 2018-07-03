fdd3108d134a193dcd24d690afa3567ad05a8030
Alsey Coleman Miller <alseycmiller@gmail.com>
2018-07-02 20:07:03 -0500

    Fixed indentation

--- Sources/GATT/DarwinAttributes.swift
+++ Sources/GATT/DarwinAttributes.swift
@@ -9,148 +9,150 @@
 import Bluetooth
 
 #if os(macOS) || os(iOS) || os(tvOS) || (os(watchOS) && swift(>=3.2))
+
+import Foundation
+import CoreBluetooth
+
+internal protocol CoreBluetoothAttributeConvertible {
     
-    import Foundation
-    import CoreBluetooth
+    //associatedtype CoreBluetoothCentralType
+    associatedtype CoreBluetoothPeripheralType
     
-    internal protocol CoreBluetoothAttributeConvertible {
+    //init(_ CoreBluetooth: CoreBluetoothCentralType)
+    func toCoreBluetooth() -> CoreBluetoothPeripheralType
+}
+
+#if os(macOS) || os(iOS) // watchOS and tvOS only support Central mode
+extension GATT.Service: CoreBluetoothAttributeConvertible {
+    
+    func toCoreBluetooth() -> CBMutableService {
         
-        //associatedtype CoreBluetoothCentralType
-        associatedtype CoreBluetoothPeripheralType
+        let service = CBMutableService(type: uuid.toCoreBluetooth(), primary: primary)
         
-        //init(_ CoreBluetooth: CoreBluetoothCentralType)
-        func toCoreBluetooth() -> CoreBluetoothPeripheralType
+        service.characteristics = characteristics.map { $0.toCoreBluetooth() }
+        
+        return service
     }
+}
+
+extension GATT.Characteristic: CoreBluetoothAttributeConvertible {
     
-    #if os(macOS) || os(iOS) // watchOS and tvOS only support Central mode
-    extension GATT.Service: CoreBluetoothAttributeConvertible {
+    func toCoreBluetooth() -> CBMutableCharacteristic {
         
-        func toCoreBluetooth() -> CBMutableService {
-            
-            let service = CBMutableService(type: uuid.toCoreBluetooth(), primary: primary)
-            
-            service.characteristics = characteristics.map { $0.toCoreBluetooth() }
-            
-            return service
-        }
+        let propertyRawValue = CBCharacteristicProperties.RawValue.self
+        
+        let propertiesMask = CBCharacteristicProperties(rawValue: propertyRawValue.init(properties.rawValue))
+        
+        let permissionRawValue = CBAttributePermissions.RawValue.self
+        
+        let permissionsMask = CBAttributePermissions(rawValue: permissionRawValue.init(permissions.rawValue))
+        
+        // http://stackoverflow.com/questions/29228244/issues-in-creating-writable-characteristic-in-core-bluetooth-framework#29229075
+        // Characteristics with cached values must be read-only
+        // Must set nil as value.
+        
+        let characteristic = CBMutableCharacteristic(type: uuid.toCoreBluetooth(), properties: propertiesMask, value: nil, permissions: permissionsMask)
+        
+        characteristic.descriptors = descriptors.map { $0.toCoreBluetooth() }
+        
+        return characteristic
     }
+}
+
+extension GATT.Descriptor: CoreBluetoothAttributeConvertible {
     
-    extension GATT.Characteristic: CoreBluetoothAttributeConvertible {
+    func toCoreBluetooth() -> CBDescriptor {
         
-        func toCoreBluetooth() -> CBMutableCharacteristic {
-            
-            let propertyRawValue = CBCharacteristicProperties.RawValue.self
-            
-            let propertiesMask = CBCharacteristicProperties(rawValue: propertyRawValue.init(properties.rawValue))
-            
-            let permissionRawValue = CBAttributePermissions.RawValue.self
-            
-            let permissionsMask = CBAttributePermissions(rawValue: permissionRawValue.init(permissions.rawValue))
-            
-            // http://stackoverflow.com/questions/29228244/issues-in-creating-writable-characteristic-in-core-bluetooth-framework#29229075
-            // Characteristics with cached values must be read-only
-            // Must set nil as value.
-            
-            let characteristic = CBMutableCharacteristic(type: uuid.toCoreBluetooth(), properties: propertiesMask, value: nil, permissions: permissionsMask)
-            
-            characteristic.descriptors = descriptors.map { $0.toCoreBluetooth() }
-            
-            return characteristic
-        }
+        /*
+         Only the Characteristic User Description and Characteristic Presentation Format descriptors are currently supported. The Characteristic Extended Properties and Client Characteristic Configuration descriptors will be created automatically upon publication of the parent service, depending on the properties of the characteristic itself
+         
+         e.g.
+         ```
+         Assertion failure in -[CBMutableDescriptor initWithType:value:], /SourceCache/CoreBluetooth_Sim/CoreBluetooth-59.3/CBDescriptor.m:25
+         ```
+         */
+        
+        guard let descriptor = DarwinDescriptor(uuid: uuid, data: value)
+            else { fatalError("Unsupported \(CBDescriptor.self) \(uuid)") }
+        
+        return CBMutableDescriptor(descriptor)
     }
+}
+
+#endif
+
+internal protocol CoreBluetoothBitmaskConvertible: BitMaskOption {
     
-    extension GATT.Descriptor: CoreBluetoothAttributeConvertible {
-        
-        func toCoreBluetooth() -> CBDescriptor {
-            
-            /*
-             Only the Characteristic User Description and Characteristic Presentation Format descriptors are currently supported. The Characteristic Extended Properties and Client Characteristic Configuration descriptors will be created automatically upon publication of the parent service, depending on the properties of the characteristic itself
-             
-             e.g.
-             ```
-             Assertion failure in -[CBMutableDescriptor initWithType:value:], /SourceCache/CoreBluetooth_Sim/CoreBluetooth-59.3/CBDescriptor.m:25
-             ```
-             */
-            
-            guard let descriptor = DarwinDescriptor(uuid: uuid, data: value)
-                else { fatalError("Unsupported \(CBDescriptor.self) \(uuid)") }
-            
-            return CBMutableDescriptor(descriptor)
-        }
-    }
-        
-    #endif
+    associatedtype CoreBluetoothBitmaskType: OptionSet
     
-    internal protocol CoreBluetoothBitmaskConvertible: BitMaskOption {
-        
-        associatedtype CoreBluetoothBitmaskType: OptionSet
-        
-        /// Values that are supported in CoreBluetooth
-        static var coreBluetoothValues: [Self] { get }
-        
-        /// Convert from CoreBluetooth bitmask.
-        static func from(coreBluetooth: CoreBluetoothBitmaskType) -> BitMaskOptionSet<Self>
-    }
+    /// Values that are supported in CoreBluetooth
+    static var coreBluetoothValues: [Self] { get }
     
-    extension GATT.CharacteristicProperty: CoreBluetoothBitmaskConvertible {
+    /// Convert from CoreBluetooth bitmask.
+    static func from(coreBluetooth: CoreBluetoothBitmaskType) -> BitMaskOptionSet<Self>
+}
+
+extension GATT.CharacteristicProperty: CoreBluetoothBitmaskConvertible {
+    
+    typealias CoreBluetoothBitmaskType = CBCharacteristicProperties
+    
+    static let coreBluetoothValues: [GATT.CharacteristicProperty] = [
+        .broadcast,
+        .read,
+        .writeWithoutResponse,
+        .write,
+        .notify,
+        .indicate,
+        .signedWrite,
+        .extendedProperties
+    ]
+    
+    static func from(coreBluetooth: CoreBluetoothBitmaskType) -> BitMaskOptionSet<GATT.CharacteristicProperty> {
         
-        typealias CoreBluetoothBitmaskType = CBCharacteristicProperties
+        let bitmask = coreBluetooth.rawValue
         
-        static let coreBluetoothValues: [GATT.CharacteristicProperty] = [
-            .broadcast,
-            .read,
-            .writeWithoutResponse,
-            .write,
-            .notify,
-            .indicate,
-            .signedWrite,
-            .extendedProperties
-        ]
+        var convertedValues = BitMaskOptionSet<GATT.CharacteristicProperty>()
         
-        static func from(coreBluetooth: CoreBluetoothBitmaskType) -> BitMaskOptionSet<GATT.CharacteristicProperty> {
+        for possibleValue in GATT.CharacteristicProperty.coreBluetoothValues {
             
-            let bitmask = coreBluetooth.rawValue
+            let rawValue = CoreBluetoothBitmaskType.RawValue(possibleValue.rawValue)
             
-            var convertedValues = BitMaskOptionSet<GATT.CharacteristicProperty>()
-            
-            for possibleValue in GATT.CharacteristicProperty.coreBluetoothValues {
+            if rawValue & bitmask == rawValue {
                 
-                let rawValue = CoreBluetoothBitmaskType.RawValue(possibleValue.rawValue)
-                
-                if rawValue & bitmask == rawValue {
-                    
-                    convertedValues.insert(possibleValue)
-                }
+                convertedValues.insert(possibleValue)
             }
-            
-            return convertedValues
         }
+        
+        return convertedValues
     }
+}
+
+extension ATT.AttributePermission: CoreBluetoothBitmaskConvertible {
     
-    extension ATT.AttributePermission: CoreBluetoothBitmaskConvertible {
+    typealias CoreBluetoothBitmaskType = CBAttributePermissions
+    
+    static let coreBluetoothValues: [ATT.AttributePermission] = [.read, .write] + ATT.AttributePermission.encrypt
+    
+    static func from(coreBluetooth: CoreBluetoothBitmaskType) -> BitMaskOptionSet<ATT.AttributePermission> {
         
-        typealias CoreBluetoothBitmaskType = CBAttributePermissions
+        let bitmask = coreBluetooth.rawValue
         
-        static let coreBluetoothValues: [ATT.AttributePermission] = [.read, .write] + ATT.AttributePermission.encrypt
+        var convertedValues = BitMaskOptionSet<ATT.AttributePermission>()
         
-        static func from(coreBluetooth: CoreBluetoothBitmaskType) -> BitMaskOptionSet<ATT.AttributePermission> {
+        for possibleValue in ATT.AttributePermission.coreBluetoothValues {
             
-            let bitmask = coreBluetooth.rawValue
+            let rawValue = CoreBluetoothBitmaskType.RawValue(possibleValue.rawValue)
             
-            var convertedValues = BitMaskOptionSet<ATT.AttributePermission>()
-            
-            for possibleValue in ATT.AttributePermission.coreBluetoothValues {
+            if rawValue & bitmask == rawValue {
                 
-                let rawValue = CoreBluetoothBitmaskType.RawValue(possibleValue.rawValue)
-                
-                if rawValue & bitmask == rawValue {
-                    
-                    convertedValues.insert(possibleValue)
-                }
+                convertedValues.insert(possibleValue)
             }
-            
-            return convertedValues
         }
+        
+        return convertedValues
+    }
+}
+
 // MARK: - Swift Compatibility
 
 extension CBCharacteristic {
