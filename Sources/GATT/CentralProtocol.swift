a2cb82d6b0c04939f56f131ce9a5ea24c0e29be7
Alsey Coleman Miller <alseycmiller@gmail.com>
2020-06-09 15:53:53 -0500

    Added `AsynchronousCentral`

--- Sources/GATT/CentralProtocol.swift
+++ Sources/GATT/CentralProtocol.swift
@@ -30,6 +30,8 @@ import Bluetooth
 @available(*, deprecated, message: "Please migrate to 'CombineCentral'")
 public typealias CentralProtocol = SynchronousCentral
 
+// MARK: - Synchronous
+
 /// GATT Central Manager
 ///
 /// Implementation varies by operating system.
@@ -38,6 +40,8 @@ public protocol SynchronousCentral: class {
     associatedtype Peripheral: Peer
     
     associatedtype Advertisement: AdvertisementDataProtocol
+    
+    associatedtype AttributeID: Hashable
         
     var log: ((String) -> ())? { get set }
     
@@ -50,6 +54,8 @@ public protocol SynchronousCentral: class {
     
     var isScanning: Bool { get }
     
+    var scanningChanged: ((Bool) -> ())? { get set }
+    
     func connect(to peripheral: Peripheral, timeout: TimeInterval) throws
     
     func disconnect(peripheral: Peripheral)
@@ -60,48 +66,153 @@ public protocol SynchronousCentral: class {
     
     func discoverServices(_ services: [BluetoothUUID],
                           for peripheral: Peripheral,
-                          timeout: TimeInterval) throws -> [Service<Peripheral>]
+                          timeout: TimeInterval) throws -> [Service<Peripheral, AttributeID>]
     
     func discoverCharacteristics(_ characteristics: [BluetoothUUID],
-                                for service: Service<Peripheral>,
-                                timeout: TimeInterval) throws -> [Characteristic<Peripheral>]
+                                for service: Service<Peripheral, AttributeID>,
+                                timeout: TimeInterval) throws -> [Characteristic<Peripheral, AttributeID>]
     
-    func readValue(for characteristic: Characteristic<Peripheral>,
+    func readValue(for characteristic: Characteristic<Peripheral, AttributeID>,
                    timeout: TimeInterval) throws -> Data
     
     func writeValue(_ data: Data,
-                    for characteristic: Characteristic<Peripheral>,
+                    for characteristic: Characteristic<Peripheral, AttributeID>,
                     withResponse: Bool,
                     timeout: TimeInterval) throws
     
     func notify(_ notification: ((Data) -> ())?,
-                for characteristic: Characteristic<Peripheral>,
+                for characteristic: Characteristic<Peripheral, AttributeID>,
                 timeout: TimeInterval) throws
     
     func maximumTransmissionUnit(for peripheral: Peripheral) throws -> ATTMaximumTransmissionUnit 
 }
 
+// MARK: - Asynchronous
+
+/// Asyncronous GATT Central manager.
+public protocol AsynchronousCentral: class {
+    
+    ///
+    associatedtype Peripheral: Peer
+    
+    ///
+    associatedtype Advertisement: AdvertisementDataProtocol
+    
+    ///
+    associatedtype AttributeID: Hashable
+    
+    /// 
+    var log: ((String) -> ())? { get set }
+    
+    /// Scans for peripherals that are advertising services.
+    func scan(filterDuplicates: Bool,
+              foundDevice: @escaping (ScanData<Peripheral, Advertisement>) -> ())
+    
+    /// Stops scanning for peripherals.
+    func stopScan()
+    
+    ///
+    var isScanning: Bool { get }
+    
+    ///
+    var scanningChanged: ((Bool) -> ())? { get set }
+    
+    ///
+    func connect(to peripheral: Peripheral,
+                 timeout: TimeInterval,
+                 completion: (Result<Void, Error>) -> ())
+    
+    ///
+    func disconnect(peripheral: Peripheral)
+    
+    ///
+    func disconnectAll()
+    
+    ///
+    var didDisconnect: ((Peripheral) -> ())? { get set }
+    
+    ///
+    func discoverServices(_ services: [BluetoothUUID],
+                          for peripheral: Peripheral,
+                          timeout: TimeInterval,
+                          completion: (Result<[Service<Peripheral, AttributeID>], Error>) -> ())
+    
+    ///
+    func discoverCharacteristics(_ characteristics: [BluetoothUUID],
+                                for service: Service<Peripheral, AttributeID>,
+                                timeout: TimeInterval,
+                                completion: (Result<[Characteristic<Peripheral, AttributeID>], Error>) -> ())
+    
+    ///
+    func readValue(for characteristic: Characteristic<Peripheral, AttributeID>,
+                   timeout: TimeInterval,
+                   completion: (Result<Data, Error>) -> ())
+    
+    ///
+    func writeValue(_ data: Data,
+                    for characteristic: Characteristic<Peripheral, AttributeID>,
+                    withResponse: Bool,
+                    timeout: TimeInterval,
+                    completion: (Result<Void, Error>) -> ())
+    
+    ///
+    func notify(_ notification: ((Data) -> ())?,
+                for characteristic: Characteristic<Peripheral, AttributeID>,
+                timeout: TimeInterval,
+                completion: (Result<Void, Error>) -> ())
+    
+    ///
+    func maximumTransmissionUnit(for peripheral: Peripheral,
+                                 completion: (Result<ATTMaximumTransmissionUnit, Error>) -> ())
+}
+/*
 // MARK: - Combine Support
 
 #if canImport(Combine) || canImport(OpenCombine)
 
 /// Asyncronous GATT Central manager.
 @available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 6.0, *)
-public protocol CombineCentral: class {
+public final class CombineCentral <Central: SynchronousCentral> {
     
-    associatedtype Peripheral: Peer
+    public typealias Peripheral = Central.Peripheral
     
-    associatedtype Advertisement: AdvertisementDataProtocol
+    public typealias Advertisement = Central.Advertisement
+        
+    public typealias AttributeID = Central.AttributeID
+    
+    public let central: Central
+    
+    public var userInfo = [String: Any]()
+    
+    public var queue = DispatchQueue(label: "\(Central.self) Combine Queue")
+    
+    public init(central: Central) {
+        self.central = central
+        self.isScanning = central.isScanning
+        central.log = { [weak self] in self?.log.send($0) }
+        central.scanningChanged = { [weak self] in self?.isScanning = $0 }
+    }
     
     /// TODO: Improve logging API, use Logger?
-    var log: PassthroughSubject<String, Error> { get set }
+    public let log = PassthroughSubject<String, Error>()
     
-    var isScanning: CurrentValueSubject<Bool, Error> { get }
-    
-    var isAvailable: CurrentValueSubject<Bool, Error> { get }
+    @Published
+    public private(set) var isScanning = false
     
     /// Scans for peripherals that are advertising services.
-    func scan(filterDuplicates: Bool) -> PassthroughSubject<ScanData<Peripheral, Advertisement>, Error>
+    func scan(filterDuplicates: Bool) -> PassthroughSubject<ScanData<Peripheral, Advertisement>, Error> {
+        let subject = PassthroughSubject<ScanData<Peripheral, Advertisement>, Error>()
+        queue.async { [weak self] in
+            guard let self = self else { return }
+            do {
+                try self.central.scan(filterDuplicates: filterDuplicates, foundDevice)
+            }
+            catch {
+                
+            }
+        }
+        return subject
+    }
     
     /// Stops scanning for peripherals.
     func stopScan()
@@ -151,7 +262,7 @@ public protocol CombineCentral: class {
 }
 
 #endif
-
+*/
 // MARK: - Deprecated
 
 #if !arch(wasm32) // && canImport(Dispatch)
@@ -282,9 +393,9 @@ public protocol GATTAttribute: Hashable, Identifiable {
     var peripheral: Peripheral { get }
 }
 
-public struct Service <Peripheral: Peer> : GATTAttribute {
+public struct Service <Peripheral: Peer, ID: Hashable> : GATTAttribute {
     
-    public let identifier: UInt
+    public let id: ID
     
     public let uuid: BluetoothUUID
     
@@ -292,23 +403,23 @@ public struct Service <Peripheral: Peer> : GATTAttribute {
     
     public let isPrimary: Bool
     
-    public init(identifier: UInt,
+    public init(id: ID,
                 uuid: BluetoothUUID,
                 peripheral: Peripheral,
                 isPrimary: Bool = true) {
         
-        self.identifier = identifier
+        self.id = id
         self.uuid = uuid
         self.peripheral = peripheral
         self.isPrimary = isPrimary
     }
 }
 
-public struct Characteristic <Peripheral: Peer> : GATTAttribute {
+public struct Characteristic <Peripheral: Peer, ID: Hashable> : GATTAttribute {
     
     public typealias Property = GATT.CharacteristicProperty
     
-    public let identifier: UInt
+    public let id: ID
     
     public let uuid: BluetoothUUID
     
@@ -316,31 +427,31 @@ public struct Characteristic <Peripheral: Peer> : GATTAttribute {
     
     public let properties: BitMaskOptionSet<Property>
     
-    public init(identifier: UInt,
+    public init(id: ID,
                 uuid: BluetoothUUID,
                 peripheral: Peripheral,
                 properties: BitMaskOptionSet<Property>) {
         
-        self.identifier = identifier
+        self.id = id
         self.uuid = uuid
         self.peripheral = peripheral
         self.properties = properties
     }
 }
 
-public struct Descriptor <Peripheral: Peer>: GATTAttribute {
+public struct Descriptor <Peripheral: Peer, ID: Hashable>: GATTAttribute {
     
-    public let identifier: UInt
+    public let id: ID
     
     public let uuid: BluetoothUUID
     
     public let peripheral: Peripheral
     
-    public init(identifier: UInt,
+    public init(id: ID,
                 uuid: BluetoothUUID,
                 peripheral: Peripheral) {
         
-        self.identifier = identifier
+        self.id = id
         self.uuid = uuid
         self.peripheral = peripheral
     }
