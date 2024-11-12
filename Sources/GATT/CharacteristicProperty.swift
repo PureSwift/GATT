f9a74e58edb0bb666de9c8b635e18b51214b5a76
Alsey Coleman Miller <alseycmiller@gmail.com>
2024-11-12 18:39:22 -0500

    Add `OptionSet` conformance to `CharacteristicProperties`

--- Sources/GATT/CharacteristicProperty.swift
+++ Sources/GATT/CharacteristicProperty.swift
@@ -5,44 +5,72 @@
 //  Created by Alsey Coleman Miller on 10/23/20.
 //
 
-import Foundation
 @_exported import Bluetooth
 #if canImport(BluetoothGATT)
 @_exported import BluetoothGATT
-public typealias CharacteristicProperty = GATTCharacteristicProperty
+public typealias CharacteristicProperties = BluetoothGATT.GATTCharacteristicProperties
 #else
-/// GATT Characteristic Properties Bitfield values
-public enum CharacteristicProperty: UInt8, BitMaskOption {
+/// GATT Characteristic Properties Bitfield valuess
+public struct CharacteristicProperties: OptionSet, Hashable, Sendable {
     
-    case broadcast              = 0x01
-    case read                   = 0x02
-    case writeWithoutResponse   = 0x04
-    case write                  = 0x08
-    case notify                 = 0x10
-    case indicate               = 0x20
+    public var rawValue: UInt8
     
-    /// Characteristic supports write with signature
-    case signedWrite            = 0x40 // BT_GATT_CHRC_PROP_AUTH
+    public init(rawValue: UInt8) {
+        self.rawValue = rawValue
+    }
+}
+
+// MARK: - ExpressibleByIntegerLiteral
+
+extension CharacteristicProperties: ExpressibleByIntegerLiteral {
     
-    case extendedProperties     = 0x80
+    public init(integerLiteral value: UInt8) {
+        self.rawValue = value
+    }
 }
 
 // MARK: CustomStringConvertible
 
-extension CharacteristicProperty: CustomStringConvertible {
+extension CharacteristicProperties: CustomStringConvertible, CustomDebugStringConvertible {
     
+    #if hasFeature(Embedded)
     public var description: String {
-        
-        switch self {
-        case .broadcast:                return "Broadcast"
-        case .read:                     return "Read"
-        case .write:                    return "Write"
-        case .writeWithoutResponse:     return "Write without Response"
-        case .notify:                   return "Notify"
-        case .indicate:                 return "Indicate"
-        case .signedWrite:              return "Signed Write"
-        case .extendedProperties:       return "Extended Properties"
-        }
+        "0x" + rawValue.toHexadecimal()
     }
+    #else
+    @inline(never)
+    public var description: String {
+        let descriptions: [(CharacteristicProperties, StaticString)] = [
+            (.broadcast,            ".broadcast"),
+            (.read,                 ".read"),
+            (.write,                ".write"),
+            (.notify,               ".notify"),
+            (.indicate,             ".indicate"),
+            (.signedWrite,          ".signedWrite"),
+            (.extendedProperties,   ".extendedProperties")
+        ]
+        return buildDescription(descriptions)
+    }
+    #endif
+
+    /// A textual representation of the file permissions, suitable for debugging.
+    public var debugDescription: String { self.description }
+}
+
+// MARK: - Options
+
+public extension CharacteristicProperties {
+    
+    static var broadcast: CharacteristicProperties            { 0x01 }
+    static var read: CharacteristicProperties                 { 0x02 }
+    static var writeWithoutResponse: CharacteristicProperties { 0x04 }
+    static var write: CharacteristicProperties                { 0x08 }
+    static var notify: CharacteristicProperties               { 0x10 }
+    static var indicate: CharacteristicProperties             { 0x20 }
+    
+    /// Characteristic supports write with signature
+    static var signedWrite: CharacteristicProperties          { 0x40 } // BT_GATT_CHRC_PROP_AUTH
+    
+    static var extendedProperties: CharacteristicProperties   { 0x80 }
 }
 #endif
