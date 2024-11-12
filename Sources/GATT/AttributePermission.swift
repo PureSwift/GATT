f085ea0d19c13ff6001917ba1a6db06690894eda
Alsey Coleman Miller <alseycmiller@gmail.com>
2024-11-12 18:38:14 -0500

    Add `OptionSet` conformance to `AttributePermissions`

--- Sources/GATT/AttributePermission.swift
+++ Sources/GATT/AttributePermission.swift
@@ -5,61 +5,85 @@
 //  Created by Alsey Coleman Miller on 10/23/20.
 //
 
-import Foundation
 @_exported import Bluetooth
 #if canImport(BluetoothGATT)
 @_exported import BluetoothGATT
-public typealias AttributePermission = ATTAttributePermission
+public typealias AttributePermissions = BluetoothGATT.ATTAttributePermissions
 #else
 /// ATT attribute permission bitfield values. Permissions are grouped as
 /// "Access", "Encryption", "Authentication", and "Authorization". A bitmask of
 /// permissions is a byte that encodes a combination of these.
-public enum AttributePermission: UInt8, BitMaskOption {
+@frozen
+public struct AttributePermissions: OptionSet, Equatable, Hashable, Sendable {
+    
+    public var rawValue: UInt8
+    
+    public init(rawValue: UInt8) {
+        self.rawValue = rawValue
+    }
+}
+
+// MARK: - ExpressibleByIntegerLiteral
+
+extension AttributePermissions: ExpressibleByIntegerLiteral {
+    
+    public init(integerLiteral value: UInt8) {
+        self.rawValue = value
+    }
+}
+
+// MARK: - CustomStringConvertible
+
+extension AttributePermissions: CustomStringConvertible, CustomDebugStringConvertible {
+    
+    #if hasFeature(Embedded)
+    public var description: String {
+        "0x" + rawValue.toHexadecimal()
+    }
+    #else
+    @inline(never)
+    public var description: String {
+        let descriptions: [(AttributePermissions, StaticString)] = [
+            (.read, ".read"),
+            (.write, ".write"),
+            (.readEncrypt, ".readEncrypt"),
+            (.writeEncrypt, ".writeEncrypt"),
+            (.readAuthentication, ".readAuthentication"),
+            (.writeAuthentication, ".writeAuthentication"),
+            (.authorized, ".authorized"),
+            (.noAuthorization, ".noAuthorization"),
+        ]
+        return buildDescription(descriptions)
+    }
+    #endif
+
+    /// A textual representation of the file permissions, suitable for debugging.
+    public var debugDescription: String { self.description }
+}
+
+// MARK: - Options
+
+public extension AttributePermissions {
     
     // Access
-    case read                                       = 0x01
-    case write                                      = 0x02
+    static var read: AttributePermissions                     { 0x01 }
+    static var write: AttributePermissions                    { 0x02 }
     
     // Encryption
-    public static let encrypt                       = BitMaskOptionSet<AttributePermission>([.readEncrypt, .writeEncrypt])
-    case readEncrypt                                = 0x04
-    case writeEncrypt                               = 0x08
+    static var encrypt: AttributePermissions                  { [.readEncrypt, .writeEncrypt] }
+    static var readEncrypt: AttributePermissions              { 0x04 }
+    static var writeEncrypt: AttributePermissions             { 0x08 }
     
     // The following have no effect on Darwin
     
     // Authentication
-    public static let  authentication               = BitMaskOptionSet<AttributePermission>([.readAuthentication, .writeAuthentication])
-    case readAuthentication                         = 0x10
-    case writeAuthentication                        = 0x20
+    static var authentication: AttributePermissions           { [.readAuthentication, .writeAuthentication] }
+    static var readAuthentication: AttributePermissions       { 0x10 }
+    static var writeAuthentication: AttributePermissions      { 0x20 }
     
     // Authorization
-    case authorized                                 = 0x40
-    case noAuthorization                            = 0x80
+    static var authorized: AttributePermissions               { 0x40 }
+    static var noAuthorization: AttributePermissions          { 0x80 }
 }
 
-public extension AttributePermission {
-    
-    var name: String {
-        
-        switch self {
-        case .read: return "Read"
-        case .write: return "Write"
-        case .readEncrypt: return "Read Encrypt"
-        case .writeEncrypt: return "Write Encrypt"
-        case .readAuthentication: return "Read Authentication"
-        case .writeAuthentication: return "Write Authentication"
-        case .authorized: return "Authorized"
-        case .noAuthorization: return "No Authorization"
-        }
-    }
-}
-
-// MARK: CustomStringConvertible
-
-extension AttributePermission: CustomStringConvertible {
-    
-    public var description: String {
-        return name
-    }
-}
 #endif
